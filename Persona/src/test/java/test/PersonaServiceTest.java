40981b3a6974f4f2353b5a4a332d331f6b4178c4
Rodolfo Campos <camposer@gmail.com>
2014-09-24 10:37:08 +0200

    Cambio en la estructura del proyecto para compatibilizar con Maven (main y test). Agregadas dependencias de pruebas en el POM. Agregada implementación de prueba unitaria del servicio

---
+++ Persona/src/test/java/test/PersonaServiceTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.util.List;
+
+import model.Persona;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import service.PersonaService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext-test.xml"})
+public class PersonaServiceTest {
+	@Autowired
+	private PersonaService personaService;
+	
+	@Test
+	public void probarTodoEnLaVida() {
+		Persona p = new Persona();
+		p.setNombre("xxx");
+		p.setApellido("yyy");
+		
+		personaService.agregarPersona(p);
+		
+		// TODO: Refactorizar!!!
+		List<Persona> personas = personaService.obtenerPersonas();
+		if (personas != null) for (Persona p1 : personas) {
+			boolean condicion = p1.getNombre().equals("xxx") && 
+					p1.getApellido().equals("yyy");
+			
+			if (condicion) {
+				Assert.assertTrue(condicion);
+				personaService.eliminarPersoan(p1.getId());
+				break;
+			}
+		}
+	}
+}
